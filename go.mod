45917ed8c3c9706ab89ba056509324c51770bbfe
arthmis <arthmis20@gmail.com>
2021-04-15 20:24:23 -0400

    remember to load the env file and also check if the environment variable was actually loaded

--- go.mod
+++ go.mod
@@ -10,10 +10,11 @@ require (
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gocql/gocql v0.0.0-20200815110948-5378c8f664e9
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
-	github.com/google/uuid v1.1.2
+	github.com/google/uuid v1.2.0
 	github.com/gorilla/schema v1.1.0
 	github.com/gorilla/sessions v1.2.0
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/honeycombio/beeline-go v1.0.0
 	github.com/honeycombio/opentelemetry-exporter-go v0.16.0 // indirect
 	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/jackc/pgx v3.6.2+incompatible
@@ -26,14 +27,15 @@ require (
 	github.com/sony/sonyflake v1.0.0
 	go.opentelemetry.io/contrib/instrumentation/net/http v0.11.0 // indirect
 	go.opentelemetry.io/otel v0.19.0
-	go.opentelemetry.io/otel/exporters/otlp v0.19.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp v0.19.0
 	go.opentelemetry.io/otel/exporters/stdout v0.19.0
 	go.opentelemetry.io/otel/sdk v0.19.0
 	go.opentelemetry.io/otel/trace v0.19.0
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
-	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/grpc v1.36.1
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- opentracing-integration.go
+++ opentracing-integration.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc/credentials"
 
 	// "github.com/honeycombio/opentelemetry-exporter-go/honeycomb"
@@ -20,12 +21,18 @@ import (
 )
 
 func initTracer() func() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
 	apikey, _ := os.LookupEnv("HONEYCOMB_API_KEY")
 	dataset, _ := os.LookupEnv("HONEYCOMB_DATASET")
 
 	ctx := context.Background()
-	exporter, err := otlp.NewExporter(
-		ctx,
+	// exporter, err := otlp.NewExporter(
+	exporter := otlp.NewUnstartedExporter(
+		// ctx,
 		otlpgrpc.NewDriver(
 			otlpgrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")),
 			otlpgrpc.WithEndpoint("api.honeycomb.io:443"),
@@ -35,9 +42,22 @@ func initTracer() func() {
 			}),
 		),
 	)
+	// exporter, err := otlp.NewExporter(
+	// 	ctx,
+	// 	otlphttp.NewDriver(
+	// 		// otlpgrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")),
+	// 		otlphttp.WithEndpoint("api.honeycomb.io:443"),
+	// 		otlphttp.WithHeaders(map[string]string{
+	// 			"x-honeycomb-team":    apikey,
+	// 			"x-honeycomb-dataset": dataset,
+	// 		}),
+	// 	),
+	// )
+	// otlphttp.NewDriver(otlphttp.)
+	err = exporter.Start(ctx)
 	if err != nil {
 		// applog.Sugar.Fatal("failed to initialize http export pipeline: %v", err)
-		log.Fatalf("failed to initialize http export pipeline: %v", err)
+		log.Fatalf("failed to initialize honeycomb export pipeline: %v", err)
 	}
 	// exporter, err := stdout.NewExporter(stdout.WithPrettyPrint())
 	// if err != nil {
@@ -52,7 +72,7 @@ func initTracer() func() {
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithBatcher(exporter),
 	)
-	log.Printf("%v", provider)
+	// log.Printf("%v", provider)
 	otel.SetTracerProvider(provider)
 
 	return func() {
@@ -67,24 +87,47 @@ func main() {
 	cleanup := initTracer()
 	defer cleanup()
 
+	// ctx := context.Background()
+	// tracer := otel.Tracer("")
+	// tracerprovider := otel.GetTracerProvider()
+	// // spew.Dump(tracerprovider)
+	// log.Printf("%v", tracerprovider)
+
+	// ctx, span := tracer.Start(ctx, "doTracing")
+	// defer span.End()
+
+	// span.AddEvent("Doing Tracing!", trace.WithAttributes((attribute.Int("bogons", 100))))
+	// span.SetAttributes(attribute.Bool("isTrue", true))
+	// apikey, isThere := os.LookupEnv("HONEYCOMB_API_KEY")
+	// log.Print(isThere)
+	// dataset, _ := os.LookupEnv("HONEYCOMB_DATASET")
+	// beeline.Init(beeline.Config{
+	// 	// Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
+	// 	WriteKey: apikey,
+	// 	// The name of your app is a good choice to start with
+	// 	Dataset: dataset,
+	// })
+	// defer beeline.Close()
 	router := chi.NewRouter()
-	router.Use(doTracing)
-	// router.With(doTracing).Get("/do-thing", doThing)
-	router.Get("/do-thing", doThing)
-	// otlphttp.
+	// router.Use(doTracing)
+	// // router.With(doTracing).Get("/do-thing", doThing)
 	// router.Get("/do-thing", doThing)
-	// router.Get("/do-thing", otelhttp.NewHandler(doThing, "test"))
+	router.Get("/do-thing", doThing)
+	// // router.Get("/do-thing", otelhttp.NewHandler(doThing, "test"))
+	// _ = http.ListenAndServe(":8000", hnynethttp.WrapHandler(router))
 	_ = http.ListenAndServe(":8000", router)
 }
 
 func doThing(w http.ResponseWriter, req *http.Request) {
 	// applog.Sugar.Info("hello")
+	tracer := otel.Tracer("")
+	_, span := tracer.Start(req.Context(), "doTracing")
 	// span := trace.SpanFromContext(req.Context())
 	// tracer := otel.Tracer("doThing")
 	// var span trace.Span
 	// _, span = tracer.Start(req.Context(), "doThing")
-	// defer span.End()
-	// span.AddEvent("Hello, Finished doing thing!")
+	defer span.End()
+	span.AddEvent("Hello, Finished doing thing!")
 	// span.SetAttributes(kv.Any("id", id), kv.Any("price", price))
 	// applog.Sugar.Info("goodbye")
 
@@ -94,14 +137,15 @@ func doThing(w http.ResponseWriter, req *http.Request) {
 func doTracing(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		tracer := otel.Tracer("")
-		tracerprovider := otel.GetTracerProvider()
+		// tracerprovider := otel.GetTracerProvider()
 		// spew.Dump(tracerprovider)
-		log.Printf("%v", tracerprovider)
+		// log.Printf("%v", tracerprovider)
 
 		ctx, span := tracer.Start(req.Context(), "doTracing")
 		defer span.End()
 
 		span.AddEvent("Doing Tracing!", trace.WithAttributes((attribute.Int("bogons", 100))))
+		span.SetAttributes(attribute.Bool("isTrue", true))
 
 		next.ServeHTTP(w, req.WithContext(ctx))
 		// ww := middleware.NewWrapResponseWriter(w, req.ProtoMajor)
