dc9c241d15a922c764f726fd21361c0cadfbac92
lazypassion <25536767+lazypassion@users.noreply.github.com>
2021-04-29 17:24:47 -0400

    rewriting login test and added login test where user didn't signup at all

--- app/auth_test.go
+++ app/auth_test.go
@@ -29,38 +29,77 @@ func TestSignup(t *testing.T) {
 	}
 }
 
-func TestLogin(t *testing.T) {
-	// func(t *testing.T) {
-	// 	addUser()
-	// 	t.Cleanup(func() {
-	// 		_, err := database.PgDB.Exec(
-	// 			`DELETE FROM users;`,
-	// 		)
-	// 		if err != nil {
-	// 			applog.Sugar.Error("error deleting all users: ", err)
-	// 		}
-	// 	})
-	// }(t)
+func TestLoginWithSignup(t *testing.T) {
+	server, client, err := serverSetup()
+	t.Cleanup(func() {
+		server.Close()
+		databaseReset()
+	})
 
-	// form := url.Values{}
-	// form.Set("email", "test@gmail.com")
-	// form.Set("password", "secretpassy")
+	err = func() error { // Signs up a user
+		form := url.Values{}
+		form.Set("email", "test@gmail.com")
+		form.Set("username", "art")
+		form.Set("password", "secretpassy")
+		form.Set("confirmPassword", "secretpassy")
 
-	// encodedForm := strings.NewReader(form.Encode())
-	// req, err := http.NewRequest(http.MethodPost, "/login", encodedForm)
-	// if err != nil {
-	// 	t.Fatal("error creating new request: ", err)
-	// }
-	// req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		_, err := client.PostForm(server.URL+"/api/user/signup", form)
+		if err != nil {
+			t.Fatalf("err: %v", err)
+			return err
+		}
+		return nil
 
-	// res := httptest.NewRecorder()
-	// router := newRouter()
-	// router.ServeHTTP(res, req)
+	}()
+	if err != nil {
+		t.Errorf("Received an error when signing up a user: %v", err)
+	}
 
-	// status := res.Code
-	// if status != http.StatusSeeOther {
-	// 	t.Errorf("login endpoint returned wrong status code: got %v want %v\n", status, http.StatusSeeOther)
-	// }
+	form := url.Values{}
+	form.Set("email", "test@gmail.com")
+	form.Set("password", "secretpassy")
+
+	res, err := client.PostForm(server.URL+"/api/user/login", form)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	// TODO: ok for now this will return a 404 when successful because it redirects
+	// then request the chat page but that isn't served by the server, if it were using nginx
+	// then it wouldn't be an issue, or if it it was serving static files
+	if res.StatusCode != http.StatusNotFound {
+		t.Errorf("User art was not logged in. Received status code %v, wanted %v", res.StatusCode, http.StatusNotFound)
+	}
+
+	// checking if session was created for art
+	session, err := application.PgStore.Get(res.Request, "session-name")
+	if err != nil {
+		Sugar.Error("err getting session name: ", err)
+	}
+
+	clientName := session.Values["username"].(string)
+	if clientName != "art" {
+		t.Error("Expected client name to be art.")
+	}
+}
+
+func TestLoginWithoutSignup(t *testing.T) {
+	server, client, err := serverSetup()
+	t.Cleanup(func() {
+		server.Close()
+		databaseReset()
+	})
+
+	form := url.Values{}
+	form.Set("email", "test@gmail.com")
+	form.Set("password", "secretpassy")
+
+	res, err := client.PostForm(server.URL+"/api/user/login", form)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("User art was not created. Received status code %v, wanted %v", res.StatusCode, http.StatusOK)
+	}
 }
 
 func TestLogout(t *testing.T) {
